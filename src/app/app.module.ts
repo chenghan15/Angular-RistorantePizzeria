8cd5eb26f9862923c1f116cf211a99dc5c746179
chenghan15 <hanwfifteen@gmail.com>
2019-07-09 20:21:25 +1200

    As3t1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { FormsModule } from '@angular/forms'; 
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSliderModule } from '@angular/material/slider';
 
 import { FlexLayoutModule } from '@angular/flex-layout';
 
@@ -67,6 +68,7 @@ import { LoginComponent } from './login/login.component';
     MatSelectModule,
     MatSlideToggleModule,
     MatProgressSpinnerModule,
+    MatSliderModule,
     ReactiveFormsModule
   ],
   providers: [
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -6,6 +6,10 @@ import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 import { switchMap } from 'rxjs/operators';
 
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Comment } from '../shared/Comment';
+
+
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
@@ -18,9 +22,16 @@ export class DishdetailComponent implements OnInit {
   prev: string;
   next: string;
 
+  dishCommentForm: FormGroup;
+  dishComment: Comment;
+
   constructor(private dishservice: DishService,
     private location: Location,
-    private route: ActivatedRoute) { }
+    private route: ActivatedRoute,
+    private fb: FormBuilder) 
+    { 
+      this.createForm();
+    }
 
     ngOnInit() {
       this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
@@ -34,8 +45,29 @@ export class DishdetailComponent implements OnInit {
       this.next = this.dishIds[(this.dishIds.length + index + 1) % this.dishIds.length];
     }
 
-  goBack(): void {
-    this.location.back();    
-  }
+    goBack(): void {
+      this.location.back();    
+    }
 
+    createForm(){
+      this.dishCommentForm = this.fb.group(
+        {
+          rating: 5,
+          comment: '',
+          author: ''
+          // date: '' 
+        }
+      );
+    }
+
+    onSubmit(){
+      // this.dishComment = this.dishCommentForm.value;
+      this.dishComment.rating = this.dishCommentForm.value['rating'];
+      this.dishComment.author = this.dishCommentForm.value['author'];
+      this.dishComment.comment = this.dishCommentForm.value['comment'];
+      // this.dishComment.date = '';
+
+      console.log(this.dishComment);
+      this.dishCommentForm.reset();
+    }
 }
